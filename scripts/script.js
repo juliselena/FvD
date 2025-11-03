ab60103e76f6dcb668cf3946133f1808ec3e6314
juliselena <juliselenabuiten@icloud.com>
2025-11-03 12:33:34 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,15 @@
 
 /* CONST      */
-
-const slider = document.querySelector('#homePagina ul:first-of-type');
-const items = slider.querySelectorAll('li');
-const dotsWrapper = document.querySelector('#homePagina ul:first-of-type + div');
-
-const section = document.querySelector('#homePagina > section:nth-of-type(1)');
-const articlesContainer = section.querySelector('div');
-const btnPrev = section.querySelector('button:first-of-type');
-const btnNext = section.querySelector('button:last-of-type');
-
 const toggle = document.getElementById('theme-toggle');
 const myButton = document.querySelector('header > button:last-of-type');
-
+var openButton = document.querySelector("#menu-open");
+openButton.onclick = openMenu;
+var closeButton = document.querySelector("#menu-close");
+closeButton.onclick = closeMenu;
 
 /* FUNCTIONS  */
-//bolletjes aanmaken
-function createDots() {
-    items.forEach((_, index) => {
-        const dot = document.createElement('button');
-        dot.addEventListener('click', () => {
-            slider.scrollTo({
-                left: slider.clientWidth * index,
-                behavior: 'smooth'
-            });
-        });
-        dotsWrapper.appendChild(dot);
-    });
-}
 
-//voor de pijltjes knoppen
-function scrollPrev() {
-    articlesContainer.scrollBy({
-        left: -articlesContainer.offsetWidth / 2,
-        behavior: 'smooth'
-    });
-}
 
-function scrollNext() {
-    articlesContainer.scrollBy({
-        left: articlesContainer.offsetWidth / 2,
-        behavior: 'smooth'
-    });
-}
 //tekst grootte aanpassen
 let isLargeFont = false;
 function toggleFontSize() {
@@ -56,14 +23,19 @@ function toggleFontSize() {
     }
 }
 
+function openMenu() {
+    var menu = document.querySelector("#menu");
+    menu.classList.add("toonMenu");
+}
 
-createDots();
+function closeMenu() {
+    var menu = document.querySelector("#menu");
+    menu.classList.remove("toonMenu");
+}
+
 
 
 /* EVENT LISTENERS */
-btnPrev.addEventListener('click', scrollPrev);
-btnNext.addEventListener('click', scrollNext);
-
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
@@ -71,3 +43,5 @@ toggle.addEventListener('click', () => {
 myButton.addEventListener('click', toggleFontSize);
 
 //https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
+//https://www.w3schools.com/css/css3_variables_javascript.asp
+//developer.mozilla.org/en-US/docs/Web/CSS/CSS_cascading_variables/Using_CSS_custom_properties
